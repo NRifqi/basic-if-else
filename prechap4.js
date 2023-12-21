af5a0fc14cd9ad2834ca084c89d166bab96b82f8
Nur Rifqi I <islaminurrifqi@gmail.com>
2023-12-21 20:55:00 +0700

    new file

---
+++ prechap4.js
@@ -0,0 +1,51 @@
+// let access;
+// let age = prompt("how old are you?");
+
+// if (age > 18 ) {
+//     access = "allowed";
+// } else {
+//     access = "not allowed"
+// }
+
+// alert(access);
+
+// logic and
+// let result = true && 10;
+// console.log(result);
+
+// console.log(false && "hello world");
+// console.log(null && "hello world");
+
+// logic or
+// let result = false || 10;
+// console.log(result);
+
+// console.log(true || 10);
+// console.log(null || 5);
+// console.log(undefined || null);
+// console.log(0 || "hello world");
+
+// logic not
+// let rain = false;
+
+// if (!rain) console.log("don't use the umbrella");
+
+// if else
+// let quetion = prompt("are you okay?");
+
+// if (quetion == "ok") {
+//     alert("that's good");
+// }
+// else {
+//     alert("why?")
+// }
+
+// let treasure = prompt("how much money are you saving?");
+
+// if (treasure > 1000000000) {
+//     alert("crazy rich")
+// } else if (treasure >= 100000000 && treasure <= 200000000) {
+//     alert("that's good")
+// } else {
+//     alert("so poor")
+// }
